10f8b1e1629d42ae1b4be915aee1f06a108bcd2b
Renycbz18 <reny.chila@pucese.edu.ec>
2018-12-03 07:52:00 -0500

    3DICIEMBRE

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -10,9 +10,30 @@ namespace AppVectores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Control de Versiones");
-            Console.WriteLine("Probando la nueva version");
+           
+            VectorInicializar();
             Console.ReadKey();
+        }
+
+        public static void VectorInicializar()
+        {
+            int[] x=new int[8];
+            x[0] = 10;
+            x[1] = 2;
+            x[2] = 20;
+            x[3] = 8;
+            x[4] = 15;
+            x[5] = 1;
+            x[6] = -9;
+            x[7] = 14;
+            //IMPRIMIR EL VECTOR
+            Console.WriteLine("\nELEMENTOS DEL VECTOR\n=============================\n");
+
+            for (int i=0; i<8; i++)
+            {
+                Console.WriteLine("ELEMENTO INDICE {0} --> {1}", (i + 1), x[i]);
+            }
+
         }
     }
 }
