ebad43ed2f1f9ca7244df808df77d6cf74286bb9
Renycbz18 <45071424+Renycbz18@users.noreply.github.com>
2018-12-06 07:38:08 -0500

    Update and rename AppVectores/AppVectores/Program.cs to  AppVectores/ AppVectores/Program.cs

--- AppVectores/ AppVectores/Program.cs
+++ AppVectores/ AppVectores/Program.cs
@@ -74,7 +74,68 @@ namespace AppVectores
             Console.WriteLine("el pro,edio es  {0}", (double)s / x.Length);
         }
     }
+public static void TareaVectores()
 
+		{
+
+			int[] vec = new int[10];
+
+			int mayor = vec[0], menor = vec[0];
+
+
+			for (int i = 0; i < vec.Length; i++)
+
+			{
+
+				while (vec[i] < 1 || vec[i] > 1000)
+
+				{
+
+					Console.WriteLine("Ingrese valor {0} del vector", i + 1);
+
+					vec[i] = Int32.Parse(Console.ReadLine());
+
+				}
+
+
+			}
+
+			for (int i = 0; i < vec.Length; i++)
+
+			{
+
+				if (vec[i] > mayor)
+                    {
+
+					mayor = vec[i];
+
+				}
+
+			}
+
+			menor = mayor;
+
+			for (int i = 0; i < vec.Length; i++)
+
+			{
+
+				if (vec[i] < menor)
+
+				{
+
+					menor = vec[i];
+
+				}
+
+			}
+
+
+
+			Console.WriteLine("El numero mayor es: " + mayor);
+
+			Console.WriteLine("El numero menor es: " + menor);
+
+		
     }
 
 
